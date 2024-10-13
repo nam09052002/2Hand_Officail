4658ff5e6e1ad5bb789423c539165e35d0f672ad
Đặng Phương Nam <117804388+nam09052002@users.noreply.github.com>
2024-10-14 00:57:52 +0700

    Update

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,34 +7,37 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  loginForm: FormGroup;
-  errorMessage: string = ''; // Khai báo biến lỗi
+  ten_dang_nhap: string = '';
+  mat_khau: string = '';
+  errorMessage: string = '';
 
-  constructor(private fb: FormBuilder, private http: HttpClient) {
-    this.loginForm = this.fb.group({
-      username: ['', [Validators.required]],
-      password: ['', [Validators.required]],
-    });
-  }
+  constructor(private router: Router) {}
 
   onSubmit() {
-    if (this.loginForm.valid) {
-      this.http.post('http://localhost/api/routes/userRoutes.php', {
-        action: 'login',
-        ...this.loginForm.value
-      }).subscribe(
-        response => {
-          console.log('Login response:', response);
-          if (response && (response as any).success) { // Chuyển đổi response thành kiểu any
-            // Xử lý phản hồi từ server
+      const formData = new FormData();
+      formData.append('ten_dang_nhap', this.ten_dang_nhap);
+      formData.append('mat_khau', this.mat_khau);
+
+      fetch('http://localhost/api/users/user-login.php', {
+          method: 'POST',
+          body: formData,
+      })
+      .then(response => response.json())
+      .then(data => {
+          if (data.status === 'success') {
+              // Xử lý đăng nhập thành công
+              this.errorMessage = data.message
+              console.log(data.message);
+              // Lưu thông tin người dùng vào local storage (hoặc session storage)
+              localStorage.setItem('user', JSON.stringify(data.user));
+              // Điều hướng đến trang chủ
+              this.router.navigate(['/']); // Thay '/' bằng đường dẫn trang chủ của bạn
           } else {
-            this.errorMessage = (response as any).message || 'Đăng nhập thất bại'; // Xử lý lỗi
+              // Hiển thị thông báo lỗi
+              console.error(data.message);
+              this.errorMessage = data.message;
           }
-        },
-        error => {
-          this.errorMessage = 'Có lỗi xảy ra trong quá trình đăng nhập';
-        }
-      );
-    }
+      })
+      .catch(error => console.error('Error:', error));
   }
 }
